01ab367b49d2f4814f2a2a0c84c507e1610739c0
Justas Belevicius <jube1997@gmail.com>
2022-01-29 21:43:53 +0200

    Add reconnect

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,40 @@
-import React, { useReducer, useState } from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 import { GameContext, gameReducer } from './game/GameContext';
 import StateHandler from './StateHandler';
 import states, { State } from './States';
 import { WebSocketContext } from './WebSocketContext';
 import './App.css';
+import { Message, ReconnectRequestPayload } from 'common/src/Message';
+import { MessageType } from 'common/src/MessageType';
 
-console.log(process.env.REACT_APP_WS_HOST);
-const ws = new WebSocket(process.env.REACT_APP_WS_HOST || "");
-
+let ws = new WebSocket(process.env.REACT_APP_WS_HOST || "");
 
 function App() {
   const [state, setState] = useState(State.LOGIN);
   const [game, dispatch] = useReducer(gameReducer, {});
+
+  useEffect(() => {
+    const reconnect = () => {
+      console.log("DROPPED CONNECTION, RESTARTING");
+      setTimeout(() => {
+        ws = new WebSocket(process.env.REACT_APP_WS_HOST || "");
+        if (!game.playerId) {
+          setState(State.LOGIN);
+          return;
+        }
+        const message: Message<ReconnectRequestPayload> = {
+          type: MessageType.RECONNECT,
+          payload: {
+            id: game.playerId
+          }
+        }
+        ws.send(JSON.stringify(message));
+      }, 1000);
+    }
+    ws.addEventListener("close", reconnect);
+    return ws.removeEventListener("close", reconnect);
+  }, []);
+
   const StateComponent = states[state];
   return <WebSocketContext.Provider value={ws}>
     <GameContext.Provider value={{ state: game, dispatch }}>
